8a891283da4e4cd3f4196842e6d8580b5ed785f3
emrocode <92260849+emrocode@users.noreply.github.com>
2024-01-25 19:22:02 -0300

    Updated DEMO

--- demo.sh
+++ demo.sh
@@ -7,14 +7,14 @@ mkdir demo && cat > ./demo/index.html << EOF
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <script type="text/javascript" src="../dist/darkify.min.js"></script>
-    <title>Darkify | Demo</title>
+    <title>Darkify &#x2014; Demo</title>
   </head>
   <body>
     <div id="root">
-      <button type="button" id="btn">Button</button>
+      <button type="button" id="element">Button</button>
     </div>
     <script type="text/javascript">
-      new Darkify('#btn', { autoMatchTheme: true, useSessionStorage: true, });
+      new Darkify("#element", { useSessionStorage: true });
     </script>
   </body>
 </html>
